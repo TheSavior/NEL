e5d80445ab70ffbc3490c19b45316c3b8f4361e6
Andrew Rogers <abennobashi@gmail.com>
2013-02-12 13:34:33 -0800

    write entity sentences to file

--- src/com/cse454/nel/Main.java
+++ src/com/cse454/nel/Main.java
@@ -1,13 +1,23 @@
 package com.cse454.nel;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 
 import com.cse454.warmup.sf.SFConstants;
 import com.cse454.warmup.sf.retriever.ProcessedCorpus;
 
 public class Main {
+
+	public static final String sentencesFile = "sentences.entities";
+
 	public static void main(String[] args) {
+
+		BufferedWriter fos = null;
+
 		try {
+			fos = new BufferedWriter(new FileWriter(sentencesFile));
 			ProcessedCorpus corpus = new ProcessedCorpus();
 			Map<String, String> annotations = null;
 			int c = 0;
@@ -24,6 +34,9 @@ public class Main {
 				String[] tokens = split[1].split(" ");
 				split = annotations.get(SFConstants.STANFORDNER).split("\t");
 				String[] stanfordNer = split[1].split(" ");
+
+				StringBuffer outSentence = new StringBuffer(id + "\t");
+
 				int length = tokens.length;
 				for (int i = 0; i < length; i++) {
 					if (stanfordNer[i].length() == 1
@@ -40,12 +53,28 @@ public class Main {
 					}
 					String entity = buffer.toString();
 					String span = startIndex + ":" + i;
-					System.out.print(entity + " " + span +"\n");
+					outSentence.append(entity + " " + span + "\t");
 				}
+				outSentence.append("\n");
+				fos.write(outSentence.toString());
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
 		}
+	}
+
+	public static void usage() {
+		System.err.println("java com.cse454.nel.Main " +
+				"<keyfile>");
+		System.exit(-1);
 	}
 }
