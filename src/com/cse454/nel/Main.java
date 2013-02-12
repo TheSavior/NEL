76e177b65ad75bdc61813c87b2e304b755cc1824
Andrew Rogers <abennobashi@gmail.com>
2013-02-12 12:01:42 -0800

    moving wikiconnect into main package, adding main

--- src/com/cse454/nel/Main.java
+++ src/com/cse454/nel/Main.java
@@ -1,5 +1,51 @@
 package com.cse454.nel;
 
-public class Main {
+import java.util.Map;
 
+import com.cse454.warmup.sf.SFConstants;
+import com.cse454.warmup.sf.retriever.ProcessedCorpus;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			ProcessedCorpus corpus = new ProcessedCorpus();
+			Map<String, String> annotations = null;
+			int c = 0;
+			while (corpus.hasNext()) {
+				annotations = corpus.next();
+				if (++c % 100 == 0) {
+					System.err.print("finished reading " + c + " lines\r");
+					break;
+				}
+
+				String[] split;
+				split = annotations.get(SFConstants.TOKENS).split("\t");
+				String id = split[0];
+				String[] tokens = split[1].split(" ");
+				split = annotations.get(SFConstants.STANFORDNER).split("\t");
+				String[] stanfordNer = split[1].split(" ");
+				int length = tokens.length;
+				for (int i = 0; i < length; i++) {
+					if (stanfordNer[i].length() == 1
+							|| stanfordNer[i].equals("DATE")
+							|| stanfordNer[i].equals("PERCENT")
+							|| stanfordNer[i].equals("NUMBER")) {
+						continue;
+					}
+					int startIndex = i;
+					StringBuffer buffer = new StringBuffer(tokens[i]);
+					while (i < length && stanfordNer[i].equals(stanfordNer[i + 1])) {
+						i++;
+						buffer.append(" " + tokens[i]);
+					}
+					String entity = buffer.toString();
+					String span = startIndex + ":" + i;
+					System.out.print(entity + " " + span +"\n");
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/com/cse454/nel/WikiConnect.java
+++ src/com/cse454/nel/WikiConnect.java
@@ -1,3 +1,4 @@
+package com.cse454.nel;
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
