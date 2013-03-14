1047725635dd3315c9298bdb7c8cabba39cf5ae2
TheSavior <github@eli-white.com>
2013-03-14 01:18:35 -0700

    Added API to pull page text from wikipedia.

---
+++ src/com/cse454/nel/PullFromWikipedia.java
@@ -0,0 +1,59 @@
+package com.cse454.nel;
+
+import java.io.*;
+import java.net.*;
+
+
+public class PullFromWikipedia {
+
+	public static final String UTF8_BOM = "\uFEFF";
+	
+	public PullFromWikipedia() {
+		
+	}
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public String GetWikipediaText(String pageTitle) throws IOException {
+		String response = post("http://en.wikipedia.org/w/api.php?action=parse&format=xml&redirects=&prop=wikitext&page="+pageTitle);
+		//String response = post(apiUrl + "action=parse", "prop=text&text=" + URLEncoder.encode(markup, "UTF-8"), "parse");
+        int y = response.indexOf('>', response.indexOf("<wikitext")) + 1;
+        int z = response.indexOf("</wikitext>");
+        //System.out.println(decode(response));
+        try {
+        	return decode(response.substring(y, z));
+        }
+        catch(Exception e)
+        {
+        	return null;
+        }
+	}
+	
+	protected String decode(String in)
+    {
+        // Remove entity references. Oddly enough, URLDecoder doesn't nuke these.
+        in = in.replace("&lt;", "<").replace("&gt;", ">"); // html tags
+        in = in.replace("&amp;", "&");
+        in = in.replace("&quot;", "\"");
+        in = in.replace("&#039;", "'");
+        return in;
+    }
+	
+	protected String post(String url) throws IOException
+    {
+        URLConnection connection = new URL(url).openConnection();
+        connection.setDoOutput(true);
+        connection.connect();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+        String line;
+        StringBuilder temp = new StringBuilder(100000);
+        while ((line = in.readLine()) != null)
+        {
+            temp.append(line);
+            temp.append("\n");
+        }
+        in.close();
+        return temp.toString();
+    }
+}
