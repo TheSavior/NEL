1c3d64f175dce2ee8e5720368a08cb6c4ce3f252
TheSavior <github@eli-white.com>
2013-03-13 23:17:52 -0700

    Added tool to fill in missing pages.

---
+++ src/com/cse454/nel/PageGenerator.java
@@ -0,0 +1,67 @@
+package com.cse454.nel;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class PageGenerator {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception  {
+		WikiConnect wiki = new WikiConnect();
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true) {
+			System.out.print("Page Title: ");
+			String page = scanner.nextLine();
+			if (wiki.doesWikiPageExist(page)) {
+				System.out.print("Page already exists. Overwrite?");
+				
+				if (!continueHelper(scanner)) {
+					break;
+				}
+				// overwrite
+			}
+			
+			FileInputStream fstream = new FileInputStream("doc.txt");
+			System.out.println("Loading text: ");
+			// Get the object of DataInputStream
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			StringBuilder strLine = new StringBuilder();
+			//Read File Line By Line
+			String line;
+			while ((line = br.readLine()) != null) {
+				strLine.append(line+"\n");
+			}
+			in.close();
+			
+			System.out.println(strLine.substring(0, 200));
+			
+			System.out.print("Correct?");
+			if (!continueHelper(scanner)) {
+				break;
+			}
+			
+			wiki.AddPage(page, strLine.toString());
+			System.out.println();
+		}
+	}
+	
+	private static boolean continueHelper(Scanner scanner) {
+		String overwrite = "a";
+		while(!overwrite.startsWith("n") && !overwrite.startsWith("y")) {
+			System.out.print("(Y/N): ");
+			overwrite = scanner.nextLine().toLowerCase();
+		}
+		
+		return !overwrite.startsWith("n");
+	}
+
+}
--- src/com/cse454/nel/WikiConnect.java
+++ src/com/cse454/nel/WikiConnect.java
@@ -276,11 +276,6 @@ public class WikiConnect extends MySQLConnect {
 	}
 	
 	public void AddPage(String name, String text) throws Exception {
-		if (doesWikiPageExist(name)) {
-			System.err.println("Already Exists");
-			return;
-		}
-
 		PreparedStatement st = null;
 		ResultSet rs = null;
 
